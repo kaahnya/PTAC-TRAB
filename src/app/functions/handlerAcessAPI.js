83b1cfeab80c8a980dc3252b95ed75e1839fb286
kaahnya <100857964+kaahnya@users.noreply.github.com>
2023-08-15 19:55:16 -0300

    Update handlerAcessAPI.js

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,52 @@
 'use server'
 
-const getUserAuthenticated = (user) => {
+const lista =[
+{
+    name: "kakah",
+    email: "kakah@gmail.com",
+    password: "123",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+},
+{
+    name: "gabriwk",
+    email: "gabriwk@gmail.com",
+    password: "123",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+},
+{
+    name: "inacio",
+    email: "inacio@gmail.com",
+    password: "123",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+},
+{
+    name: "nete",
+    email: "nete@gmail.com",
+    password: "123",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+},
+{
+    name: "mudinha",
+    email: "mudinha@gmail.com",
+    password: "123",
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+}
+]
 
+const getUserAuthenticated = (user) => {
+    let usermach =[];
+
+    lista.map((user) =>{
+    if(user.email === userLogin.email && user.password === userLogin.password){
+        usermach = user
+    }
+    })
+
+    return usermach
 }
 
 const getUsers = () =>{
+    return lista
         
 }
 export { getUsers, getUserAuthenticated };
