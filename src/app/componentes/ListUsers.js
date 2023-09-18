555824a78b369cbd3b8c1c0c5046326518026a89
kakah <karina.decleva@estudante.ifms.edu.br>
2023-09-17 23:13:13 -0300

    comecei o css da pagina lista

--- src/app/componentes/ListUsers.js
+++ src/app/componentes/ListUsers.js
@@ -2,7 +2,7 @@ export default async function ListUsers({users}) {
     await new Promise((resolve) => setTimeout(resolve, 5000));
     return(
         <div>
-            {users?.map((user, index) => <p key={index}>{user.name}</p>)}
+            {users?.map((user, index) => <p key={index}>{user.name} <img src="https://images.emojiterra.com/google/noto-emoji/unicode-15/bw/512px/1f464.png" width={50}/> </p>)}
         </div>
     );
 }
--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,14 +1,18 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 import { Suspense } from "react"
 import ListUsers from "@/app/componentes/ListUsers";
+import './css.css'
 
 
 export default async function Dashboard() {
    const users = await getUsers();
     return (
-        <div>
+        <div className="carre">
             <Suspense fallback = {<p>Carregando...</p>}>
+            <div className="lista">
+                <h1>USERS</h1>
                 <ListUsers users={users}/>
+            </div>
             </Suspense>
         </div>
     );
